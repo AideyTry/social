b5869de208cfc1227dc3ecb2e577cd662b19bcca
Aiden <jason_dlb@sina.cn>
2021-12-02 16:11:59 +0800

    project init

---
+++ README.md
@@ -0,0 +1,8 @@
+<!--
+ * @Author: Aiden(戴林波)
+ * @Date: 2021-12-02 16:11:01
+ * @LastEditTime: 2021-12-02 16:11:01
+ * @LastEditors: Aiden(戴林波)
+ * @Description: 
+ * @Email: jason_dlb@sina.cn
+-->
---
+++ social-front-end/pages/index/index.vue
@@ -0,0 +1,72 @@
+<template>
+	<view class="content">
+	  <view class="list-wraper">
+		 <view class="list-item" v-for="(index, item) in list" :key="index">
+			 <text>{{item}}</text>
+		 </view>
+	  </view>
+	</view>
+</template>
+
+<script>
+	import {request} from '../../common/request.js'
+	export default {
+		data() {
+			return {
+				list: []
+			}
+		},
+		onLoad() {
+			request('https://unidemo.dcloud.net.cn/api/news', 'GET').then((data) => {
+				console.log('data1=', data)
+			}).finally(() => {
+				console.log('end11')
+			})
+			// uni.request({
+			// 	url: 'https://unidemo.dcloud.net.cn/api/news',
+			// 	method: 'GET',
+			// 	data: {},
+			// 	success: res => {
+			// 		console.log('res===', res)
+			// 	},
+			// 	fail: (err) => {
+			// 		console.log('err=', err)
+			// 	},
+			// 	complete: () => {
+			// 		console.log('complete')
+			// 	}
+			// })
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
