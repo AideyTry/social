ad739f5336e98165ccc5ae43362c3a8e7fefe121
Aiden <jason_dlb@sina.cn>
2021-12-08 18:30:14 +0800

    feat:login&iconfont

--- social-front-end/pages/login/index.vue
+++ social-front-end/pages/login/index.vue
@@ -6,15 +6,16 @@
         <view class="login">
             <form @submit="formSubmit" @reset="formReset" class="login-in">
                 <view class="row uni-form-item uni-column">
-                    <text class="col-10 title">用户名：</text>
-                    <input class="col-12 uni-input" name="username" placeholder="请输入用户名" />
+                    <text class="col-8 title">手机号：</text>
+                    <input class="col-16 uni-input" name="username" placeholder="请输入手机号" @input="onInput"/>
                 </view>
                 <view class="row uni-form-item uni-column">
-                    <text class="col-10 title">密码：</text>
-                    <input class="col-12 uni-input" name="password" placeholder="请输入密码" />
+                    <text class="col-8 title">验证码：</text>
+                    <input class="col-8 uni-input" name="password" placeholder="请输入验证码" @input="onInputCode"/>
+					<button class="col-8 code" v-if="codeVisible" :disabled="!sendCaptchaEnabled" @click="sendCaptcha">{{codeMessage}}<text v-if="!sendCaptchaEnabled">({{counterTimer}}s)</text></button>
                 </view>
                 <view class="uni-btn-v">
-                    <button form-type="submit" type="primary" class="btn-block">登录</button>
+                    <button form-type="submit" type="primary" class="btn-block" :disabled="loginVisible" >登录</button>
                 </view>
             </form>
 			<view class="cut-off-line row">
@@ -28,9 +29,14 @@
 					  <use xlink:href="#icon-weixin"></use>
 					</svg>
 				</view>
-				<view class="login-way phone">
+				<view class="login-way qq">
 					<svg class="icon login-icon" aria-hidden="true">
-					  <use xlink:href="#icon-yepian"></use>
+					  <use xlink:href="#icon-QQ"></use>
+					</svg>
+				</view>
+				<view class="login-way weibo">
+					<svg class="icon login-icon" aria-hidden="true">
+					  <use xlink:href="#icon-weibo"></use>
 					</svg>
 				</view>
 			</view>
@@ -41,9 +47,48 @@
     export default {
         data() {
             return {
+				codeVisible: false,
+				loginVisible: true,
+				sendCaptchaEnabled: true,
+				codeMessage: '获取验证码',
+				counterTimer: 60
             }
         },
         methods: {
+			onInput: function(e){
+				console.log('input e=', e)
+				if((/^1[3456789]\d{9}$/.test(e.detail.value))){
+					this.codeVisible = true
+				} else {
+					this.codeVisible = false
+				}
+			},
+			onInputCode: function(e){
+				console.log('code=', e)
+				if(/^\d{6}$/.test(e.detail.value)){
+					this.loginVisible = false
+				} else {
+					this.loginVisible = true
+				}
+			},
+			sendCaptcha: function(e){
+				console.log('button e=', e)
+				if(!this.sendCaptchaEnabled){
+					return
+				}
+				this.sendCaptchaEnabled = false
+				this.codeMessage = '重新发送'
+				const timer = setInterval(() => {
+					if(this.counterTimer <= 0){
+						this.counterTimer = 60
+						this.sendCaptchaEnabled = true
+						clearInterval(timer)
+						this.codeMessage = '获取验证码'
+						return
+					}
+					this.counterTimer--
+				}, 1000)
+			},
             formSubmit: function(e) {
 				console.log('e=', e)
                 console.log('form发生了submit事件，携带数据为：' + JSON.stringify(e.detail.value))
@@ -74,6 +119,9 @@
     .uni-form-item .title {
         padding: 20upx 0;
     }
+	.code{
+		font-size: 14upx;
+	}
 	.uni-btn-v{
 		margin-top: 20upx;
 	}
@@ -93,16 +141,16 @@
 		display: flex;
 		justify-content: center;
 	}
-	.wechat{
-		margin-right: 150upx;
+	.wechat, .qq{
+		margin-right: 100upx;
 	}
     .login-way{
-		width: 150upx;
-		height: 150upx;
+		width: 100upx;
+		height: 100upx;
 		border-radius: 50%;
 	}
 	.login-icon{
-		width: 150upx;
-		height: 150upx;
+		width: 100upx;
+		height: 100upx;
 	}
 </style>
