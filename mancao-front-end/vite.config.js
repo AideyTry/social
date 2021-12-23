dade665a0953b15a3b6e4825cd795690af910bdb
Aiden <jason_dlb@sina.cn>
2021-12-23 08:40:18 +0800

    update port

--- mancao-front-end/vite.config.js
+++ mancao-front-end/vite.config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: Aiden(戴林波)
  * @Date: 2021-12-17 17:33:16
- * @LastEditTime: 2021-12-20 14:58:41
+ * @LastEditTime: 2021-12-23 08:39:57
  * @LastEditors: Aiden(戴林波)
  * @Description: 
  * @Email: jason_dlb@sina.cn
@@ -14,7 +14,7 @@ export default defineConfig({
     uni(),
   ],
   server: {
-    port: '8088',
+    port: '80',
     proxy: {
       '/users': {
           target: 'http://localhost:3000',
