7cc928ba38d1fce132d1a0c686f40a844e873897
Aiden <jason_dlb@sina.cn>
2021-12-29 12:23:43 +0800

    feat:request.js

--- social-back-end/util/request.js
+++ social-back-end/util/request.js
@@ -1,8 +1,8 @@
 /*
  * @Author: Aiden
  * @Date: 2021-12-28 23:08:25
- * @LastEditTime: 2021-12-28 23:24:04
- * @LastEditors: Aiden
+ * @LastEditTime: 2021-12-29 12:22:27
+ * @LastEditors: Aiden(戴林波)
  * @Description: 
  */
 const axios = require('axios');
@@ -14,8 +14,8 @@ module.exports = {
     headers: {'Authorization': 'AppCode 870473fa4f224ded869486ea76dcdc13'}
   }),
   school_2: axios.create({
-    baseURL: 'https://some-domain.com/api/',
+    // baseURL: 'https://qryschool.market.alicloudapi.com/lundroid/searchUniversity',
     timeout: 1000,
-    headers: {'X-Custom-Header': 'foobar'}
+    headers: {'Authorization': 'AppCode 870473fa4f224ded869486ea76dcdc13'}
   })
 }
