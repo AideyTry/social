f591aefea22f85baec87d5ce1a9249af843dc2fd
Aiden <jason_dlb@sina.cn>
2021-12-30 09:45:40 +0800

    feat:school loading data

--- mancao-front-end/src/pages/user/info/School.vue
+++ mancao-front-end/src/pages/user/info/School.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: Aiden(戴林波)
  * @Date: 2021-12-29 14:06:11
- * @LastEditTime: 2021-12-29 18:08:26
+ * @LastEditTime: 2021-12-30 09:40:36
  * @LastEditors: Aiden(戴林波)
  * @Description: 
  * @Email: jason_dlb@sina.cn
@@ -39,16 +39,15 @@ import { getSchool } from "../../../api/user";
 let schools = ref([]);
 let pageNum = ref(1)
 let queryString = ref('')
-let finalSchools = reactive([])
 
 const searchSchool = (query, page) => {
     const params = {
     name: query,
-    page: pageNum.value,
+    page,
   }
   getSchool(params).then((data) => {
     console.log("data===", data);
-    schools.value = data.data.data;
+    schools.value = [...schools.value, ...data.data.data];
     console.log('schools=', schools)
   })
 }
@@ -57,10 +56,10 @@ const search = (e) => {
   console.log("e===", e);
   const { value } = e;
   queryString.value = value
-  searchSchool(value, pageNum.value)
+  schools.value = []
+  searchSchool(value, 1)
 };
 const onLoadMore = () => {
-    console.log('pageNum.value===', pageNum.value)
   pageNum.value++
   console.log('pageNum.value===', pageNum.value)
   searchSchool(queryString.value, pageNum.value)
