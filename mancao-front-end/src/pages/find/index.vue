141dcd63e0d40b13daf49698cedc1dfb14b39a23
Aiden <jason_dlb@sina.cn>
2022-01-09 21:04:04 +0800

    feat:title

--- mancao-front-end/src/pages/find/index.vue
+++ mancao-front-end/src/pages/find/index.vue
@@ -1,6 +1,14 @@
+<!--
+ * @Author: Aiden(戴林波)
+ * @Date: 2021-12-17 17:50:13
+ * @LastEditTime: 2022-01-09 21:01:56
+ * @LastEditors: Aiden(戴林波)
+ * @Description: 
+ * @Email: jason_dlb@sina.cn
+-->
 <template>
 	<view>
-		
+		待上线
 	</view>
 </template>
 
