d45f69eb6af5e369a52520a7c292a311cca790c4
Aiden <jason_dlb@sina.cn>
2022-01-13 16:42:33 +0800

    verifyCommit.js

---
+++ scripts/verifyCommit.js
@@ -0,0 +1,34 @@
+/*
+ * @Author: Aiden(戴林波)
+ * @Date: 2022-01-13 16:39:58
+ * @LastEditTime: 2022-01-13 16:41:17
+ * @LastEditors: Aiden(戴林波)
+ * @Description: 
+ * @Email: jason_dlb@sina.cn
+ */
+// Invoked on the commit-msg git hook by yorkie.
+
+const chalk = require('chalk')
+const msg = require('fs')
+  .readFileSync('.git/COMMIT_EDITMSG', 'utf-8')
+  .trim()
+
+const commitRE = /^(revert: )?(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(\(.+\))?: .{1,50}/
+console.log('msg===', msg)
+if (!commitRE.test(msg)) {
+  console.log()
+  console.error(
+    `  ${chalk.bgRed.white(' ERROR ')} ${chalk.red(
+      `invalid commit message format.`
+    )}\n\n` +
+      chalk.red(
+        `  Proper commit message format is required for automated changelog generation. Examples:\n\n`
+      ) +
+      `    ${chalk.green(`feat(compiler): add 'comments' option`)}\n` +
+      `    ${chalk.green(
+        `fix(v-model): handle events on blur (close #28)`
+      )}\n\n` +
+      chalk.red(`  See .github/commit-convention.md for more details.\n`)
+  )
+  process.exit(1)
+}
