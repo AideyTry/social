b4c0efd9dae02ee76773301909f424f639fdb78d
Aiden <jason_dlb@sina.cn>
2022-01-19 22:10:22 +0800

    feat: video -> video.js

--- src/pages/index/VideoDetail.vue
+++ src/pages/index/VideoDetail.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: Aiden(戴林波)
  * @Date: 2022-01-16 21:17:51
- * @LastEditTime: 2022-01-19 18:16:55
+ * @LastEditTime: 2022-01-19 22:07:41
  * @LastEditors: Aiden(戴林波)
  * @Description: 
  * @Email: jason_dlb@sina.cn
@@ -17,7 +17,9 @@
 <script>
 import { ref, onMounted } from "vue";
 import { videoDetail } from "@/api/hobby";
-import Hls from "hls.js";
+// import Hls from "hls.js";
+// import 'videojs-contrib-hls';
+import Video from "video.js";
 export default {
   onLoad: function(options) {
     console.log("options===", options);
@@ -36,26 +38,43 @@ export default {
     /**
      * @description: 通过hls将m3u8转化为video可播放的资源地址
      * @param {*}
-     * @Author: 
+     * @Author:
      * @return {*}
-     */    
-    const getM3u8Resorce = () => {
-      const video = document.querySelector("#video");
-      const videoSrc = "https://www.mancao.social/video/output.m3u8";
-      console.log('Hls===', Hls)
-      if (Hls.isSupported()) {
-        var hls = new Hls();
-        hls.loadSource(videoSrc);
-        hls.attachMedia(video);
-        console.log('hls====', hls)
-      } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
-        video.src = videoSrc;
-      }
-      console.log("video=", video);
-    }
+     */
+
+    // const getM3u8Resorce = () => {
+    //   const video = document.querySelector("#video");
+    //   const videoSrc = "https://www.mancao.social/video/output.m3u8";
+    //   console.log('Hls===', Hls)
+    //   if (Hls.isSupported()) {
+    //     var hls = new Hls();
+    //     hls.loadSource(videoSrc);
+    //     hls.attachMedia(video);
+    //     console.log('hls====', hls)
+    //   } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
+    //     video.src = videoSrc;
+    //   }
+    //   console.log("video=", video);
+    // }
+
+    let nowPlayVideoUrl = "https://www.mancao.social/video/output.m3u8";
+
+    const m3u8Video = () => {
+      const currentInstance = Video(document.querySelector("#video"), {
+        autoplay: true, // 是否自动播放
+        controls: true, // 是否显示控件
+      });
+
+      currentInstance.src({
+        src: nowPlayVideoUrl,
+        type: "application/x-mpegURL", // 修改这个type值
+      });
+    };
+
     onMounted(() => {
       getVideoDetail(props.id);
-      getM3u8Resorce()
+      // getM3u8Resorce()
+      m3u8Video()
     });
     return {
       videInfo,
