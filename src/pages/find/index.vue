a336a3c95b0ef9df8d331e3bd60e26480e5647f4
Aiden <jason_dlb@sina.cn>
2022-01-19 16:17:19 +0800

    feat: handle video

--- src/pages/find/index.vue
+++ src/pages/find/index.vue
@@ -1,30 +1,80 @@
 <!--
  * @Author: Aiden(戴林波)
  * @Date: 2021-12-17 17:50:13
- * @LastEditTime: 2022-01-09 21:01:56
+ * @LastEditTime: 2022-01-19 10:54:06
  * @LastEditors: Aiden(戴林波)
  * @Description: 
  * @Email: jason_dlb@sina.cn
 -->
 <template>
-	<view>
-		待上线
-	</view>
+  <view>
+    <uni-file-picker
+      fileMediatype="video"
+      @select="onSelect"
+      @progress="progress"
+      @success="success"
+      @fail="fail"
+    />
+    <view>
+      <button @tap="uploadVideo">上传视频</button>
+      <view v-if="src"><video :src="src"></video></view>
+    </view>
+  </view>
 </template>
 
-<script>
-	export default {
-		data() {
-			return {
-				
-			}
-		},
-		methods: {
-			
-		}
-	}
+<script setup>
+import { ref } from "vue";
+
+let src = ref("");
+
+const uploadVideo = () => {
+  uni.chooseVideo({
+    sourceType: ["camera", "album"],
+    success: function(res) {
+      console.log("res===", res);
+      src.value = res.tempFilePath;
+	    uni.uploadFile({
+    url: "http://127.0.0.1:3000/files/uploadVideo",
+    filePath: res.tempFilePath,
+    name: "file",
+	fileType: 'video',
+    formData: {
+      user: "test",
+    },
+    success: (uploadFileRes) => {
+      console.log("uploadFileRes===", uploadFileRes);
+    },
+  });
+    },
+  });
+};
+const success = (e) => {
+  console.log("e===", e);
+};
+const onSelect = (e) => {
+  console.log("select ", e);
+  const { tempFilePaths } = e;
+  src.value = tempFilePaths[0];
+  console.log('tempFilePath===', tempFilePaths[0])
+  uni.uploadFile({
+    url: "http://127.0.0.1:3000/files/uploadVideo",
+    filePath: tempFilePaths[0],
+    name: "file",
+	fileType: 'video',
+    formData: {
+      user: "test",
+    },
+    success: (uploadFileRes) => {
+      console.log("uploadFileRes===", uploadFileRes);
+    },
+  });
+};
+const progress = (e) => {
+  console.log("progress=", e);
+};
+const fail = (err) => {
+  console.log("err=", err);
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/pages/index/VideoDetail.vue
+++ src/pages/index/VideoDetail.vue
@@ -1,26 +1,25 @@
 <!--
  * @Author: Aiden(戴林波)
  * @Date: 2022-01-16 21:17:51
- * @LastEditTime: 2022-01-17 09:35:06
+ * @LastEditTime: 2022-01-19 16:15:18
  * @LastEditors: Aiden(戴林波)
  * @Description: 
  * @Email: jason_dlb@sina.cn
 -->
 <template>
   <div class="video-detail">
-    <view class="title">{{videInfo.title}}</view>
-    <text class="abstract">{{videInfo.abstract}}</text>
-    <video controls="controls" preload="meta">
-      <source :src="videInfo.src">
-    </video>
+    <view class="title">{{ videInfo.title }}</view>
+    <text class="abstract">{{ videInfo.abstract }}</text>
+    <video id="video"></video>
   </div>
 </template>
 
 <script>
 import { ref, onMounted } from "vue";
 import { videoDetail } from "@/api/hobby";
+import Hls from "hls.js";
 export default {
-  onLoad: function (options) {
+  onLoad: function(options) {
     console.log("options===", options);
   },
   setup(props, context) {
@@ -38,6 +37,19 @@ export default {
     };
     onMounted(() => {
       getVideoDetail(props.id);
+      const videoId = document.querySelector("#video");
+      // const videoSrc = "https://www.mancao.social/video/output.m3u8";
+      const videoSrc = "https://www.mancao.social/video/output.m3u8";
+      console.log('Hls===', Hls)
+      if (Hls.isSupported()) {
+        var hls = new Hls();
+        hls.loadSource(videoSrc);
+        hls.attachMedia(video);
+        console.log('hls====', hls)
+      } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
+        video.src = videoSrc;
+      }
+      console.log("videoId=", videoId);
     });
     return {
       videInfo,
@@ -48,17 +60,17 @@ export default {
 
 <style lang="scss" scoped>
 .video-detail {
-    padding: 20rpx;
+  padding: 20rpx;
   .title {
     font-size: 40rpx;
     color: #666;
     margin: 16rpx 0 0 0;
     font-weight: normal;
   }
-  .abstract{
+  .abstract {
     //   font-size: 24rpx;
-      font-family: Arial, Helvetica, sans-serif;
-      color: #666;
+    font-family: Arial, Helvetica, sans-serif;
+    color: #666;
   }
 }
 </style>
