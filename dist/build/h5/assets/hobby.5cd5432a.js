0329d11bfe75e00960df97634da4f6bc40ee49be
Aiden <jason_dlb@sina.cn>
2022-01-24 18:06:50 +0800

    chore: build:h5

--- dist/build/h5/assets/hobby.5cd5432a.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index.f78fe6a4.js";const s=()=>e("/users/getVideoList","GET"),t=s=>e("/users/getVideoDetail","GET",s);export{s as g,t as v};
---
+++ dist/build/h5/assets/hobby.5f16a90b.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index.e582ec7f.js";const s=()=>e("/users/getVideoList","GET"),t=s=>e("/users/getVideoDetail","GET",s);export{s as g,t as v};
