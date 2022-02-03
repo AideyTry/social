c22fb0472e8c76f8e53668b07dc02986ce01509e
Aiden <jason_dlb@sina.cn>
2022-02-03 23:19:47 +0800

    feat: add api uploadFile

--- src/api/file.js
+++ src/api/file.js
@@ -1,7 +1,7 @@
 /*
  * @Author: Aiden(戴林波)
  * @Date: 2021-12-22 11:15:02
- * @LastEditTime: 2021-12-22 11:15:02
+ * @LastEditTime: 2022-02-03 22:52:55
  * @LastEditors: Aiden(戴林波)
  * @Description: 
  * @Email: jason_dlb@sina.cn
@@ -10,4 +10,8 @@ import { request } from '../utils/request.js'
 
 export const fileUpload = (params) => {
 	return request(`/users/uploadFile`,'POST', params)
+}
+
+export const uploadVideo = (params) => {
+	return request(`/files/uploadVideo`, 'POST', params)
 }
