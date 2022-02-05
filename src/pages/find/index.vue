e5fe5c65837641c324130eff24b7d885f0a0ab58
Aiden <jason_dlb@sina.cn>
2022-02-05 23:06:37 +0800

    feat: progress

--- src/pages/find/index.vue
+++ src/pages/find/index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: Aiden(戴林波)
  * @Date: 2021-12-17 17:50:13
- * @LastEditTime: 2022-02-04 23:38:53
+ * @LastEditTime: 2022-02-05 23:02:29
  * @LastEditors: Aiden(戴林波)
  * @Description: 
  * @Email: jason_dlb@sina.cn
@@ -15,10 +15,11 @@
       @success="success"
       @fail="fail"
     />
-    <progress :percent="progressPercent" active :fontSize="24" activeColor="#00f"/>
+    <progress :percent="progressPercent" active :fontSize="24" activeColor="#00f" show-info stroke-width="3"/>
+<!-- <progress :percent="80" active :fontSize="24" activeColor="#00f" show-info stroke-width="3"/> -->
     <view>
       <button @tap="uploadVideo">上传视频</button>
-      <video :src="src"></video>
+      <!-- <video :src="src"></video> -->
     </view>
   </view>
 </template>
@@ -27,7 +28,7 @@
 import { ref } from "vue";
 
 let src = ref("");
-let progressPercent = ref('')
+let progressPercent = ref('0');
 
 const uploadVideo = () => {
   uni.chooseVideo({
@@ -72,9 +73,9 @@ const onSelect = (e) => {
   });
 
   uploadTask.onProgressUpdate((res) => {
-    console.log('res===', res)
+    console.log("res===", res);
     console.log("上传进度" + res.progress);
-    progressPercent.value = res.progress
+    progressPercent.value = res.progress;
     console.log("已经上传的数据长度" + res.totalBytesSent);
     console.log("预期需要上传的数据总长度" + res.totalBytesExpectedToSend);
 
@@ -84,12 +85,28 @@ const onSelect = (e) => {
     // }
   });
 };
-const progress = (e) => {
-  console.log("progress=", e);
-};
+// const progress = (e) => {
+//   console.log("progress=", e);
+// };
 const fail = (err) => {
   console.log("err=", err);
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>    
+// .progress-box {
+//         display: flex;
+//         height: 50rpx;
+//         margin-bottom: 60rpx;
+//     }
+//     .uni-icon {
+//         line-height: 1.5;
+//     }
+//     .progress-cancel {
+//         margin-left: 40rpx;
+//     }
+    
+//     .progress-control button{
+//         margin-top: 20rpx;
+//     }
+</style>
--- src/pages/message/index.vue
+++ src/pages/message/index.vue
@@ -1,14 +1,14 @@
 <!--
  * @Author: Aiden(戴林波)
  * @Date: 2021-12-17 17:50:38
- * @LastEditTime: 2022-01-09 21:02:05
+ * @LastEditTime: 2022-02-05 23:00:26
  * @LastEditors: Aiden(戴林波)
  * @Description: 
  * @Email: jason_dlb@sina.cn
 -->
 <template>
 	<view>
-		待上线
+<progress :percent="80" active :fontSize="24" activeColor="#00f" show-info stroke-width="3"/>
 	</view>
 </template>
 
