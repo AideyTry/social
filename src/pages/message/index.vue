3d86281df809172b90c7370e58ffd03a18a2ffd1
Aiden <jason_dlb@sina.cn>
2022-02-09 21:11:19 +0800

    feat: message

--- src/pages/message/index.vue
+++ src/pages/message/index.vue
@@ -1,14 +1,14 @@
 <!--
  * @Author: Aiden(戴林波)
  * @Date: 2021-12-17 17:50:38
- * @LastEditTime: 2022-02-05 23:00:26
+ * @LastEditTime: 2022-02-09 21:10:56
  * @LastEditors: Aiden(戴林波)
  * @Description: 
  * @Email: jason_dlb@sina.cn
 -->
 <template>
 	<view>
-<progress :percent="80" active :fontSize="24" activeColor="#00f" show-info stroke-width="3"/>
+		待上线
 	</view>
 </template>
 
