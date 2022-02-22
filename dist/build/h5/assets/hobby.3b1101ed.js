febef18eba8aa7cffed8bb07ba2e4740c8eebacc
Aiden <jason_dlb@sina.cn>
2022-02-22 21:32:31 +0800

    feat: production

--- dist/build/h5/assets/hobby.3b1101ed.js
+++
@@ -1,1 +0,0 @@
-import{r as s}from"./index.c101858f.js";const e=e=>s("/users/getVideoList","GET",e),t=e=>s("/users/getVideoDetail","GET",e);export{e as g,t as v};
---
+++ dist/build/h5/assets/hobby.8468fbb6.js
@@ -0,0 +1,1 @@
+import{r as s}from"./index.ca74f807.js";const e=e=>s("/users/getVideoList","GET",e),t=e=>s("/users/getVideoDetail","GET",e);export{e as g,t as v};
