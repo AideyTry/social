9cd185d7103837a8e39820e820dcfba5cd6d36ba
Aiden <jason_dlb@sina.cn>
2022-03-05 03:34:05 +0800

    feat: build

--- dist/build/h5/assets/hobby.22c36fee.js
+++
@@ -1,1 +0,0 @@
-import{r as s}from"./index.abf20ebc.js";const e=e=>s("/users/getVideoList","GET",e),a=e=>s("/users/getVideoDetail","GET",e),o=e=>(console.log("params=",e),s("/files/getHobbyList","GET",e)),t=e=>s("/files/getHobbyDetail","GET",e);export{o as a,t as b,e as g,a as v};
