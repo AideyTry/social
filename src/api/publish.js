ca316878595573af868be0003cdb23d22e9d688e
Aiden <jason_dlb@sina.cn>
2022-03-06 12:06:43 +0800

    feat: like

--- src/api/publish.js
+++ src/api/publish.js
@@ -1,7 +1,7 @@
 /*
  * @Author: Aiden(戴林波)
  * @Date: 2022-03-01 23:21:49
- * @LastEditTime: 2022-03-01 23:21:49
+ * @LastEditTime: 2022-03-05 15:25:25
  * @LastEditors: Aiden(戴林波)
  * @Description: 
  * @Email: jason_dlb@sina.cn
@@ -11,4 +11,9 @@ import { request } from '../utils/request.js'
 export const getPublish = (params) => {
 	console.log('params===', params)
 	return request(`/publishs/getPublish`,'GET', params)
+}
+
+export const getMylikes = (params) => {
+	console.log('params===', params)
+	return request(`/publishs/getMylikes`,'GET', params)
 }
--- src/pages/user/index.vue
+++ src/pages/user/index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: Aiden(戴林波)
  * @Date: 2021-12-17 17:52:36
- * @LastEditTime: 2022-03-01 23:34:51
+ * @LastEditTime: 2022-03-05 15:33:34
  * @LastEditors: Aiden(戴林波)
  * @Description: 
  * @Email: jason_dlb@sina.cn
@@ -22,34 +22,38 @@
       </view>
     </view>
     <view class="fans-wraper">
-      <view class="follows"><text>关注</text>&nbsp;<text>{{ follows }}</text></view>
-      <view class="fans"><text>粉丝</text>&nbsp;<text>{{ fans }}</text></view>
+      <view class="follows"
+        ><text>关注</text>&nbsp;<text>{{ follows }}</text></view
+      >
+      <view class="fans"
+        ><text>粉丝</text>&nbsp;<text>{{ fans }}</text></view
+      >
     </view>
     <view class="publish-wraper">
-      <view class="my-publishs" @click="changeTab">
+      <view class="my-publishs" @click="changeTab(0)">
         <text>我发布的</text>
-        <view :class="{active: activeIndex}"></view>
+        <view :class="{ active: activeIndex === 0 }"></view>
       </view>
-      <view class="my-likes" @click="changeTab">
+      <view class="my-likes" @click="changeTab(1)">
         <text>我喜欢的</text>
-        <view :class="{active: !activeIndex}"></view>
+        <view :class="{ active: activeIndex === 1 }"></view>
       </view>
     </view>
     <view>
-      <InfoList :list="publishs" v-if="publishs.length"/>
+      <InfoList :list="publishs" v-if="publishs.length" />
     </view>
     <button @click="onLogout" style="margin-top: 20rpx;">退出</button>
   </view>
 </template>
 
 <script setup>
-import { ref, computed, onMounted } from "vue";
+import { ref, computed, onMounted, watchEffect } from "vue";
 import { useStore } from "vuex";
 import { removeToken } from "../../utils/auth";
 import { getFollows, getFans } from "@/api/communication.js";
-import { getPublish } from "@/api/publish.js"
+import { getPublish, getMylikes } from "@/api/publish.js";
 
-import InfoList from '@/pages/components/InfoList.vue'
+import InfoList from "@/pages/components/InfoList.vue";
 
 const defaultAvatar = "/static/images/default_avatar.png";
 const store = useStore();
@@ -82,37 +86,54 @@ let getFollowsData = () => {
   });
 };
 let getFansData = () => {
-    getFans().then((data) => {
+  getFans().then((data) => {
     console.log("getfans =", data);
     if (data.data.code === 200) {
       fans.value = data.data.fans;
     }
   });
-}
+};
 
 // publish
-let activeIndex = ref(true)
-let publishs = ref([])
+let activeIndex = ref(0);
+let publishs = ref([]);
 
 const getPublishData = () => {
-  getPublish().then(data => {
-    console.log('data getPublish=', data)
-    if(data.data.code === 200){
-      publishs.value = data.data.data
+  getPublish().then((data) => {
+    console.log("data getPublish=", data);
+    if (data.data.code === 200) {
+      publishs.value = data.data.data;
     }
-  })
-}
+  });
+};
 
-const changeTab = () => {
-  activeIndex.value = !activeIndex.value
-}
+// 喜爱
+let likes = ref([]);
+const getMylikesData = () => {
+  getMylikes().then((data) => {
+    console.log("data likes=", data);
+    if (data.data.code === 200) {
+      likes.value = data.data.data;
+    }
+  });
+};
 
+const changeTab = (flag) => {
+  activeIndex.value = flag;
+};
 
+watchEffect(() => {
+  if (activeIndex.value === 0) {
+    getPublishData();
+  } else {
+    getMylikesData();
+  }
+}, activeIndex.value);
 
 onMounted(() => {
   getFollowsData();
-  getFansData()
-  getPublishData()
+  getFansData();
+  getPublishData();
 });
 </script>
 
@@ -145,48 +166,49 @@ onMounted(() => {
   font-size: 12rpx;
 }
 
-  .fans-wraper {
-    display: flex;
-    justify-content: center;
-  }
-  .follows {
-  }
-  .fans {
-    margin-left: 20rpx;
-    padding-left: 20rpx;
+.fans-wraper {
+  display: flex;
+  justify-content: center;
+}
+.follows {
+}
+.fans {
+  margin-left: 20rpx;
+  padding-left: 20rpx;
+  position: relative;
+}
+.fans:before {
+  content: "";
+  height: 80%;
+  position: absolute;
+  top: 10%;
+  left: 0;
+  border-left: 1px solid #ccc;
+}
+.publish-wraper {
+  display: flex;
+  justify-content: center;
+  margin: 20rpx 0;
+  padding-top: 20rpx;
+  border-top: 1rpx solid #ccc;
+  // border-bottom: 1rpx solid #ccc;
+  .my-publishs,
+  .my-likes {
+    font-size: 28rpx;
     position: relative;
   }
-  .fans:before {
-    content: "";
-    height: 80%;
-    position: absolute;
-    top: 10%;
-    left: 0;
-    border-left: 1px solid #ccc;
+  .my-likes {
+    margin-left: 40rpx;
   }
-  .publish-wraper{
-    display: flex;
-    justify-content: center;
-    margin: 20rpx 0;
-    padding-top: 20rpx;
-    border-top: 1rpx solid #ccc;
-    // border-bottom: 1rpx solid #ccc;
-    .my-publishs, .my-likes{
-      font-size: 28rpx;
-      position: relative;
-    }
-    .my-likes{
-      margin-left: 40rpx;
-    }
-    .active{
-          position: absolute;
+  .active {
+    position: absolute;
     left: 50%;
-    transform: translate(-50%,0);
+    transform: translate(-50%, 0);
     bottom: -10rpx;
     width: 100%;
     height: 0.1875rem;
     border-radius: 0.125rem;
-    background-color: #FF8200;
-    }
+    background-color: #ff8200;
   }
+}
 </style>
