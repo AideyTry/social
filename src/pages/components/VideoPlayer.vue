dfff2df3828018cbf1040d35b99479726ffdc7ed
Aiden <jason_dlb@sina.cn>
2022-03-19 23:55:02 +0800

    feat: add VideoPlayer

---
+++ src/pages/components/VideoPlayer.vue
@@ -0,0 +1,65 @@
+<!--
+ * @Author: Aiden(戴林波)
+ * @Date: 2022-03-19 14:23:05
+ * @LastEditTime: 2022-03-19 23:50:59
+ * @LastEditors: Aiden(戴林波)
+ * @Description: 
+ * @Email: jason_dlb@sina.cn
+-->
+<template>
+  <view class="video-wraper">
+    <!-- #ifdef H5 -->
+    <video id="video" ref="refdVideo" :src="src" :poster="poster" class="video"></video>
+    <!-- #endif -->
+  </view>
+</template>
+
+<script setup>
+import { ref, watch, onMounted } from "vue";
+// import Hls2 from "hls.js";
+
+const props = defineProps({
+  options: {
+    type: Object,
+    default: {
+      src: "",
+      poster: ''
+    },
+  },
+});
+
+const src = ref("");
+const poster = ref("")
+
+const videoInit = () => {
+  src.value = props.options.src;
+  poster.value = props.options.poster
+};
+
+watch(
+  () => {
+    return props.options;
+  },
+  (state) => {
+    videoInit();
+  },
+  {
+    deep: true,
+  }
+);
+
+onMounted(() => {
+    videoInit();
+});
+</script>
+
+<style scoped lang="scss">
+.video-wraper {
+  // width: 400rpx;
+  // height: 300rpx;
+  .video{
+    width: 100%;
+    height: 554rpx;
+  }
+}
+</style>
--- src/pages/index/VideoDetail.vue
+++ src/pages/index/VideoDetail.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: Aiden(戴林波)
  * @Date: 2022-01-16 21:17:51
- * @LastEditTime: 2022-03-06 20:01:58
+ * @LastEditTime: 2022-03-19 23:48:00
  * @LastEditors: Aiden(戴林波)
  * @Description: 
  * @Email: jason_dlb@sina.cn
@@ -10,15 +10,7 @@
   <div class="video-detail">
     <view class="title">{{ videInfo.title }}</view>
     <text class="abstract">{{ videInfo.abstract }}</text>
-    <!-- <video id="video"></video> -->
-    <vue3VideoPlay
-      :width="options.width"
-      :height="options.height"
-      title="冰河世纪"
-      :src="options.src"
-      :type="options.type"
-      :autoPlay="false"
-    />
+    <VideoPlayer :options="options"></VideoPlayer>
     <uni-list>
     <uni-list-item :note="item.section" v-for="item in videInfo.sections" :key="item.id">
       <template v-slot:body>
@@ -32,8 +24,11 @@
 <script>
 import { ref, onMounted, reactive } from "vue";
 import { videoDetail } from "@/api/hobby";
-import Hls from "hls.js";
+import VideoPlayer from "@/pages/components/VideoPlayer.vue";
 export default {
+  components: {
+    VideoPlayer
+  },
   onLoad: function(options) {
     console.log("options===", options);
   },
@@ -48,38 +43,14 @@ export default {
         }
       });
     };
-    /**
-     * @description: 通过hls将m3u8转化为video可播放的资源地址
-     * @param {*}
-     * @Author:
-     * @return {*}
-     */
-
-    const getM3u8Resorce = () => {
-      const video = document.querySelector("#video");
-      const videoSrc = "https://social-1308251497.cos.ap-guangzhou.myqcloud.com/video/695c8c8a78f7e9a184a17db72e734a63/695c8c8a78f7e9a184a17db72e734a63.m3u8";
-      console.log("Hls===", Hls);
-      if (Hls.isSupported()) {
-        var hls = new Hls();
-        hls.loadSource(videoSrc);
-        hls.attachMedia(video);
-        console.log("hls====", hls);
-      } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
-        video.src = videoSrc;
-      }
-      console.log("video=", video);
-    };
 
     const options = reactive({
-      width: '100%',
-      height: '280px',
+      poster: '//img2.chinadaily.com.cn/images/202201/13/61e00901a310cdd3d826b064.jpeg',
       src: "https://social-1308251497.cos.ap-guangzhou.myqcloud.com/video/695c8c8a78f7e9a184a17db72e734a63/695c8c8a78f7e9a184a17db72e734a63.m3u8", //视频源
-      type: "m3u8", //视频类型
     });
 
     onMounted(() => {
       getVideoDetail(props.id);
-      // getM3u8Resorce()
     });
     return {
       videInfo,
