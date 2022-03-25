e33857ede6b8846e70a5afd82e670166058789ca
Aiden <jason_dlb@sina.cn>
2022-03-25 16:54:58 +0800

    feat: build

---
+++ dist/build/h5/assets/pages-message-index.f7cf0194.js
@@ -0,0 +1,1 @@
+import{o as e,c as r,w as a,N as o,S as s}from"./vendor.be971eef.js";import{_ as t}from"./plugin-vue_export-helper.21dcd24c.js";var d=t({data:()=>({}),methods:{}},[["render",function(t,d,n,u,f,l){const p=s;return e(),r(p,null,{default:a((()=>[o(" 待上线 ")])),_:1})}]]);export{d as default};
--- dist/build/h5/assets/uni-app.es.1961639d.js
+++
@@ -1,1 +0,0 @@
-import{a2 as r}from"./vendor.0e999c25.js";function o(o,e){return r(o)?e:o}export{o as r};
