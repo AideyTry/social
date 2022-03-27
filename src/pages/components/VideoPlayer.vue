8940a55a2e76a0d6a890b623a4ed902da22d725e
Aiden <jason_dlb@sina.cn>
2022-03-27 19:29:44 +0800

    feat: build:h5

--- src/pages/components/VideoPlayer.vue
+++ src/pages/components/VideoPlayer.vue
@@ -1,16 +1,14 @@
 <!--
  * @Author: Aiden(戴林波)
  * @Date: 2022-03-19 14:23:05
- * @LastEditTime: 2022-03-27 16:37:38
+ * @LastEditTime: 2022-03-27 19:27:39
  * @LastEditors: Aiden(戴林波)
  * @Description: 
  * @Email: jason_dlb@sina.cn
 -->
 <template>
   <view class="video-wraper">
-    <!-- #ifdef H5 -->
     <video id="video" ref="refdVideo" :src="srcObj.src" :poster="srcObj.poster" class="video"></video>
-    <!-- #endif -->
   </view>
 </template>
 
