ef7dfd17d42cfae47602bf4aacdd7955122f382e
Aiden <jason_dlb@sina.cn>
2022-04-12 23:09:44 +0800

    feat: build:h5

---
+++ dist/build/h5/assets/publish.c4b1d371.js
@@ -0,0 +1,1 @@
+import{r as s}from"./index.c6bf79e9.js";const e=e=>(console.log("params===",e),s("/publishs/getPublish","GET",e)),l=e=>s("/publishs/deletePublish","POST",e),o=e=>(console.log("params===",e),s("/publishs/getMylikes","GET",e));export{o as a,l as d,e as g};
