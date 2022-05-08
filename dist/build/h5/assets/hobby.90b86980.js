3176b02d0a19d81bca127b7b4de87c307fcbea0c
Aiden <jason_dlb@sina.cn>
2022-05-08 11:24:36 +0800

    feat: ws url

---
+++ dist/build/h5/assets/hobby.90b86980.js
@@ -0,0 +1,2 @@
+import{r as s}from"./index.203acf82.js";const e=e=>s("/users/getVideoList","GET",e),a=e=>s("/users/getVideoDetail","GET",e),o=e=>(console.log("params=",e),s("/files/getHobbyList","GET",e)),t=e=>s("/files/getHobbyDetail","GET",e);export{o as a,t as b,e as g,a as v};
+//# sourceMappingURL=hobby.90b86980.js.map
