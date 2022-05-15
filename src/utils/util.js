d27482767852252ee127eff33b8fa359047de61a
Aiden <jason_dlb@sina.cn>
2022-05-15 15:52:24 +0800

    feat: date show

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,7 +1,7 @@
 /*
  * @Author: Aiden(戴林波)
  * @Date: 2022-02-10 16:27:29
- * @LastEditTime: 2022-05-14 00:26:02
+ * @LastEditTime: 2022-05-15 15:50:43
  * @LastEditors: Aiden(戴林波)
  * @Description: 
  * @Email: jason_dlb@sina.cn
@@ -40,18 +40,27 @@ export function formatDate(createTime){
 }
 
 export const formatMsgDate = (timestamp) => {
-    const now = new Date(timestamp);
-    const year = now.getFullYear();
-    const month = now.getMonth() + 1;
-    const date = now.getDate();
-    const hour = now.getHours();
-    let minute= now.getMinutes();
-    if (minute.toString().length === 1) minute = "0" + minute;
-    let second = now.getSeconds();
-    if (second.toString().length === 1) second = "0" + second;
-    const str1 = year + "-" + month + "-" + date;
-    // const str2 = hour + ":" + minute + ":" + second
-    const str2 = hour + ":" + minute;
-    // return [year, month, date, str1, str2];
-    return '1'
+    const week = (w) => {
+        switch(w){
+            case '1': return '一';
+            case '2': return '二';
+            case '3': return '三';
+            case '4': return '四';
+            case '5': return '五';
+            case '6': return '六';
+            case '7': return '日';
+        }
+    }
+    var a = moment(new Date(), 'YYYY-MM-DD hh:mm:ss');
+    var b = moment(new Date(timestamp), 'YYYY-MM-DD hh:mm:ss');
+    const result = a.diff(b, 'days')
+    if(result <= 1){
+       return moment(timestamp).format('H:mm');
+    } else if(result <=2){
+        return `昨天${moment(timestamp).format('H:mm')}`;
+    } else if(result <= 7){
+        return `星期${week(moment(timestamp).format('E'))} ${moment(timestamp).format('H:mm')}`;
+    }else {
+        return moment(timestamp).format('YYYY年M月D日 H:mm');
+    }
   };
