8b69680689b7df4e5746a02135c4917bbac6f7eb
Aiden <jason_dlb@sina.cn>
2022-05-16 20:13:20 +0800

    feat: build:h5

---
+++ dist/build/h5/assets/pages-hobby-index.6639f763.js
@@ -0,0 +1,2 @@
+import{o as r,c as e,Q as a}from"./vendor.ae4da609.js";import{_ as o}from"./plugin-vue_export-helper.21dcd24c.js";var t=o({data:()=>({}),methods:{}},[["render",function(o,t,s,d,n,p){const c=a;return r(),e(c)}]]);export{t as default};
+//# sourceMappingURL=pages-hobby-index.6639f763.js.map
---
+++ dist/build/h5/assets/publish.026827bc.js
@@ -0,0 +1,2 @@
+import{r as s}from"./index.4fc2b9ce.js";const l=l=>(console.log("params===",l),s("/publishs/getPublish","GET",l)),e=l=>s("/publishs/deletePublish","POST",l),a=l=>s("/publishs/updatePublish","POST",l,"application/x-www-form-urlencoded"),i=l=>s("/publishs/updateVideoPublish","POST",l),o=l=>(console.log("params===",l),s("/publishs/getMylikes","GET",l));export{o as a,a as b,e as d,l as g,i as u};
+//# sourceMappingURL=publish.026827bc.js.map
