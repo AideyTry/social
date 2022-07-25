2a93fb5f179a940fa4f01c5a095066f4d134e226
Aiden <jason_dlb@sina.cn>
2022-07-25 20:59:17 +0800

    feat: build:h5

---
+++ dist/build/h5/assets/publish.f83e46c8.js
@@ -0,0 +1,2 @@
+import{P as s}from"./index.11d2ceab.js";const l=l=>(console.log("params===",l),s("/publishs/getPublish","GET",l)),a=l=>s("/publishs/deletePublish","POST",l),e=l=>s("/publishs/updatePublish","POST",l,"application/x-www-form-urlencoded"),i=l=>s("/publishs/updateVideoPublish","POST",l),o=l=>(console.log("params===",l),s("/publishs/getMylikes","GET",l));export{o as a,e as b,a as d,l as g,i as u};
+//# sourceMappingURL=publish.f83e46c8.js.map
--- dist/build/h5/assets/uni-app.es.186d8cb4.js
+++
@@ -1,2 +0,0 @@
-import{J as r}from"./index.4f21874d.js";function n(n,o){return r(n)?o:n}export{n as r};
-//# sourceMappingURL=uni-app.es.186d8cb4.js.map
